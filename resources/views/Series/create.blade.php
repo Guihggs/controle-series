de620175df5f9648fefa625192fe3b9b88ab3732
Guilherme Alves <guilherme19961985@hotmail.com>
2022-11-07 22:29:40 -0300

    07-11

--- resources/views/Series/create.blade.php
+++ resources/views/Series/create.blade.php
@@ -1,6 +1,4 @@
-<x-layout title="Nova Série">  
-    <x-series action="route('series.store')" :nome="old('nome')" :update="false" />
-    
+<x-layout title="Nova Série">
     <form action="{{ route('series.store') }}" method="post">
         @csrf
 
@@ -8,29 +6,29 @@
             <div class="col-8">
                 <label for="nome" class="form-label">Nome:</label>
                 <input type="text"
-                        autofocus
-                        id="nome"
-                        name="nome"
-                        class="form-control"
-                        value="{{ old('nome') }}">
+                       autofocus
+                       id="nome"
+                       name="nome"
+                       class="form-control"
+                       value="{{ old('nome') }}">
             </div>
 
             <div class="col-2">
                 <label for="seasonsQty" class="form-label">Nº Temporadas:</label>
                 <input type="text"
-                        id="seasonsQty"
-                        name="seasonsQty"
-                        class="form-control"
-                        value="{{ old('seasonsQty') }}">
+                       id="seasonsQty"
+                       name="seasonsQty"
+                       class="form-control"
+                       value="{{ old('seasonsQty') }}">
             </div>
 
             <div class="col-2">
                 <label for="episodesPerSeason" class="form-label">Eps / Temporada:</label>
                 <input type="text"
-                        id="episodesPerSeason"
-                        name="episodesPerSeason"
-                        class="form-control"
-                        value="{{ old('episodesPerSeason') }}">
+                       id="episodesPerSeason"
+                       name="episodesPerSeason"
+                       class="form-control"
+                       value="{{ old('episodesPerSeason') }}">
             </div>
         </div>
 
